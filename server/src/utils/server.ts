b1f4a4fba6d2f0c5fdcab54a81017641f47e7510
Caolan <caolan.fanning@gmail.com>
2025-03-18 16:50:29 -0400

    add packages

--- server/src/utils/server.ts
+++ server/src/utils/server.ts
@@ -1,15 +1,42 @@
-import fastify from 'fastify';
+import fastify, { FastifyReply, FastifyRequest } from 'fastify';
 import { logger } from './logger';
 import { applicationRoutes } from '../modules/applications/applications.routes';
 import { userRoutes } from '../modules/users/users.routes';
 import { roleRoutes } from '../modules/roles/roles.routes';
+import guard from 'fastify-guard';
+import jwt from 'jsonwebtoken';
 
 export async function buildServer() {
 	const app = fastify({
 		logger,
 	});
 
-	// Register Plugins
+	// app.addHook(
+	// 	'onRequest',
+	// 	async function (request: FastifyRequest, reply: FastifyReply) {
+	// 		const authHeader = request.headers.authorization;
+	// 		if (!authHeader) {
+	// 			return;
+	// 		}
+
+	// 		try {
+	// 			const token = authHeader.replace('Bearer', '');
+
+	// 			const decoded = jwt.verify(token, 'secret');
+
+	// 			const user = decoded;
+	// 		} catch (error) {}
+	// 	},
+	// );
+
+	// // Register Plugins
+	// app.register(guard, {
+	// 	requestProperty: 'user',
+	// 	scopeProperty: 'scopes',
+	// 	errorHandler: (request: FastifyRequest, reply: FastifyReply) => {
+	// 		return reply.send("You can't do that");
+	// 	},
+	// });
 
 	// Register Routes
 	app.register(applicationRoutes, {
